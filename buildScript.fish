e2bc318e5792d6338b8b1b98f9e122fae307528d
bangyan wang <wangbangyan@gmail.com>
2021-03-08 03:42:36 +0800

    Update build script

--- buildScript.fish
+++ buildScript.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
-conda activate gem5
+# conda activate gem5
 scons ./build/(string upper $argv[1])/gem5.$argv[2] -j64 CC=gcc-10 CXX=g++-10 CCFLAGS_EXTRA=-std=c++20 MARSHAL_LDFLAGS_EXTRA=-fno-lto LDFLAGS_EXTRA=-fno-lto PYTHON_CONFIG=/usr/bin/python3-config
